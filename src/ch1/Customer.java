f8edec4ca9c7df38a4365955e836ad14a5019765
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-30 01:10:49 -0500

    Replace Temp with Query

--- src/ch1/Customer.java
+++ src/ch1/Customer.java
@@ -20,20 +20,30 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
 
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
         }
 
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
 
         return result;
     }
