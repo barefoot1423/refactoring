119e164bbdd643e7f01b94d68d0a7130e25475ab
Hyunwoo Cho <barefoot1423@gmail.com>
2020-02-05 00:06:26 -0500

    move methods

--- src/ch1/Rental.java
+++ src/ch1/Rental.java
@@ -1,7 +1,7 @@
 package ch1;
 
 public class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -17,35 +17,12 @@ public class Rental {
         return daysRented;
     }
 
-    double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return result;
+    int getFrequentRenterPoints() {
+        return movie.getFrequentRenterPoints(daysRented);
     }
 
-    int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+    double getCharge() {
+        return movie.getCharge(daysRented);
     }
 
 }
