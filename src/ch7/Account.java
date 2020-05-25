753df05bef3b257f2cbea0b7b53200956459469f
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-25 01:38:22 -0400

    done Move Method

--- src/ch7/Account.java
+++ src/ch7/Account.java
@@ -4,23 +4,13 @@ public class Account {
     private AccountType type;
     private int daysOverdrawn;
 
-    double overdraftCharge() {
-        if (type.isPremium()) {
-            double result = 10;
-            if (daysOverdrawn > 7) {
-                result += (daysOverdrawn - 7) * 0.85;
-            }
-            return result;
-        } else {
-            return daysOverdrawn * 1.75;
-        }
-    }
-
     double backCharge() {
         double result = 4.5;
         if (daysOverdrawn > 0) {
-            result += overdraftCharge();
+            result += type.overdraftCharge(daysOverdrawn);
         }
+
         return result;
     }
+
 }
--- src/ch7/AccountType.java
+++ src/ch7/AccountType.java
@@ -2,7 +2,21 @@ package ch7;
 
 public class AccountType {
 
+    double overdraftCharge(int daysOverdrawn) {
+        if (isPremium()) {
+            double result = 10;
+            if (daysOverdrawn > 7) {
+                result += (daysOverdrawn - 7) * 0.85;
+            }
+
+            return result;
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
+
     boolean isPremium() {
         return true;
     }
+
 }
