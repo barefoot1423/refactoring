d1c362b84beb23b41fe2b85a4292dc9ce7b36e7c
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-28 11:59:55 -0400

    Start Move Field

--- src/ch7/Account.java
+++ src/ch7/Account.java
@@ -3,6 +3,7 @@ package ch7;
 public class Account {
     private AccountType type;
     private int daysOverdrawn;
+    private double interestRate;
 
     double backCharge() {
         double result = 4.5;
@@ -11,6 +12,10 @@ public class Account {
         }
 
         return result;
+    }
+
+    double interestForAmount_days(double amount, int days) {
+        return interestRate * amount * days / 365;
     }
 
 }
