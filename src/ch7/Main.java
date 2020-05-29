98f8cfea99bcb5a414494e9586940d44a953c1db
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-29 11:56:31 -0400

    Done Inline Class

---
+++ src/ch7/Main.java
@@ -0,0 +1,10 @@
+package ch7;
+
+public class Main {
+    public static void main(String[] args) {
+        Person martin = new Person();
+        martin.getOfficeTelephone().setAreaCode("781");
+
+        martin.setOfficeAreaCode("781");
+    }
+}
--- src/ch7/Person.java
+++ src/ch7/Person.java
@@ -3,6 +3,15 @@ package ch7;
 public class Person {
 
     private String name;
+
+    public TelephoneNumber getOfficeTelephone() {
+        return officeTelephone;
+    }
+
+    public void setOfficeTelephone(TelephoneNumber officeTelephone) {
+        this.officeTelephone = officeTelephone;
+    }
+
     private TelephoneNumber officeTelephone = new TelephoneNumber();
 
     public String getName() {
@@ -15,6 +24,14 @@ public class Person {
 
     public void setOfficeAreaCode(String officeAreaCode) {
         officeTelephone.setAreaCode(officeAreaCode);
+    }
+
+    String getNumber() {
+        return officeTelephone.getOfficeNumber();
+    }
+
+    void setNumber(String number) {
+        officeTelephone.setOfficeNumber(number);
     }
 
 }
