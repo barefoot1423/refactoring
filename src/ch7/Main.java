2b4d5d2b95bd0d87165cbf6a25e888159623072f
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-29 12:06:29 -0400

    Start Hide Delegate

--- src/ch7/Main.java
+++ src/ch7/Main.java
@@ -2,9 +2,8 @@ package ch7;
 
 public class Main {
     public static void main(String[] args) {
-        Person martin = new Person();
-        martin.getOfficeTelephone().setAreaCode("781");
+        Person john = new Person();
 
-        martin.setOfficeAreaCode("781");
+        john.getDepartment().getManager();
     }
 }
--- src/ch7/Person.java
+++ src/ch7/Person.java
@@ -2,36 +2,10 @@ package ch7;
 
 public class Person {
 
-    private String name;
+    Department department;
 
-    public TelephoneNumber getOfficeTelephone() {
-        return officeTelephone;
-    }
-
-    public void setOfficeTelephone(TelephoneNumber officeTelephone) {
-        this.officeTelephone = officeTelephone;
-    }
-
-    private TelephoneNumber officeTelephone = new TelephoneNumber();
-
-    public String getName() {
-        return name;
-    }
-
-    public String getOfficeAreaCode() {
-        return officeTelephone.getAreaCode();
-    }
-
-    public void setOfficeAreaCode(String officeAreaCode) {
-        officeTelephone.setAreaCode(officeAreaCode);
-    }
-
-    String getNumber() {
-        return officeTelephone.getOfficeNumber();
-    }
-
-    void setNumber(String number) {
-        officeTelephone.setOfficeNumber(number);
+    public Department getDepartment() {
+        return department;
     }
 
 }
