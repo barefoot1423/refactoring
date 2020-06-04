42680c46203e9ad48d36b44122ee5eead4b47446
Hyunwoo Cho <barefoot1423@gmail.com>
2020-06-03 22:24:59 -0400

    clean up code

--- src/ch7/Main.java
+++ src/ch7/Main.java
@@ -3,7 +3,6 @@ package ch7;
 public class Main {
     public static void main(String[] args) {
         Person john = new Person();
-
         john.getDepartment().getManager();
         john.department.getManager();
     }
