e379fd466ef030dd385882e708708915f7f55c80
Hyunwoo Cho <barefoot1423@gmail.com>
2020-06-06 23:40:16 -0400

    clean up code

--- src/ch7/Main.java
+++ src/ch7/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         Person john = new Person();
+
         john.getDepartment().getManager();
         john.department.getManager();
-
 
     }
 }
